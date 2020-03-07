9e843e75d252a5e7306eb5e50bfe6aa8ce5c3f3a
fidansadirli <mikayil.yusubov@gmail.com>
2020-03-07 12:31:20 +0400

    --1

--- src/main/java/DAO/Booking.java
+++ src/main/java/DAO/Booking.java
@@ -1,4 +1,14 @@
 package DAO;
 
+import java.util.List;
+
 public class Booking {
+    private int idFlight;
+    private int idBooking;
+    private String username;
+    private String namePassenger;
+    private String lastnamePassenger;
+    private List<Passenger> passengers;
+    private List<Flight> flights;
+
 }
