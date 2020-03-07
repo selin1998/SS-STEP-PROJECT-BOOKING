d0873342ad6a04f5686c3917192725be5a003293
fidansadirli <mikayil.yusubov@gmail.com>
2020-03-07 14:04:22 +0400

    --4

--- src/main/java/DAO/Booking.java
+++ src/main/java/DAO/Booking.java
@@ -1,28 +1,57 @@
 package DAO;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Booking {
-    final int idFlight;
-    final int idBooking;
+    int idFlight;
+    int idBooking;
     String username;
     String namePassenger;
     String lastnamePassenger;
     List<Passenger> passengers;
     List<Flight> flights;
 
-    public Booking(int idFlight, int idBooking, String username, String namePassenger, String lastnamePassenger, List<Passenger> passengers, List<Flight> flights) {
+    public Booking(int idFlight, int idBooking, String username, String namePassenger, String lastnamePassenger)  //, List<Passenger> passengers, List<Flight> flights
+     {
         this.idFlight = idFlight;
         this.idBooking = idBooking;
         this.username = username;
         this.namePassenger = namePassenger;
         this.lastnamePassenger = lastnamePassenger;
-        this.passengers = passengers;
-        this.flights = flights;
+//        this.passengers = passengers;
+//        this.flights = flights;
     }
 
-    public String toString() {
+    public static Booking parse(String text) {
+        String[] content = text.split("|");
+        return new Booking(
+                Integer.parseInt(content[0]),
+                Integer.parseInt(content[1]),
+                content[2],
+                content[3],
+                content[4]
+//                content[5],
+//                content[6]
+        );
+    }
+
+    public String displayBookingInfo() {
         return String.format("| %-10d |%-10s |%-10s |%-15s |%-10d |%-10s|",
                 idFlight, idBooking, username, namePassenger, lastnamePassenger, passengers, flights);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+        return idFlight == booking.idFlight &&
+                idBooking == booking.idBooking;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idFlight, idBooking);
     }
 }
--- src/main/java/DAO/BookingDAO.java
+++ src/main/java/DAO/BookingDAO.java
@@ -1,4 +1,59 @@
 package DAO;
 
-public class BookingDAO {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class BookingDAO implements DAO<Booking> {
+    String filename;
+
+    @Override
+    public Optional<Booking> get(int BookingId) {
+        return getAll().stream().filter(x -> x.idBooking == BookingId).findFirst();
+    }
+
+    @Override
+    public List<Booking> getAll() {
+        try {
+            return new BufferedReader(new FileReader(new File(filename))).lines()
+                    .map(s -> Booking.parse(s))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("No bookings were found");
+        }
+    }
+
+    @Override
+    public void delete(int index) {
+
+    }
+
+    @Override
+    public void loadData(Booking booking) {
+
+    }
+
+    @Override
+    public void readData(Booking booking) {
+
+    }
+
+    @Override
+    public void saveData(Booking booking) {
+
+    }
+
+    @Override
+    public void delete(Booking booking) {
+
+    }
+
+    @Override
+    public void create(Booking booking) {
+
+    }
 }
