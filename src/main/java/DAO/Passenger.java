8be79f34ad91719df949ec260843186148a1fdb4
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-03-09 15:12:34 +0400

    -- Delete Passenger members from DaoBooking Branch

--- src/main/java/DAO/Passenger.java
+++ src/main/java/DAO/Passenger.java
@@ -1,26 +1,4 @@
 package DAO;
 
 public class Passenger {
-    private static int counter = 1;
-
-    int id;
-    String firstName;
-    String lastName;
-
-    public Passenger(String firstName, String lastName) {
-        this.id = counter++;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public Passenger(int id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String displayPassengerInfo() {
-        return String.format("Passenger %d: %s %s", id, firstName.toUpperCase(), lastName.toUpperCase());
-    }
-
 }
