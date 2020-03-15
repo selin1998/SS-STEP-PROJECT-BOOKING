49050e769d9ea83fa8890e3eee666774efab6fb8
selinhabiboghlu <shebipoghlu2019@ada.edu.az>
2020-03-15 10:49:46 +0400

    DaoBookingChanges and master merged, test classes now appear

--- src/main/java/DAO/Booking.java
+++ src/main/java/DAO/Booking.java
@@ -29,7 +29,12 @@ public class Booking implements Serializable {
         this.passenger = passenger;
         this.flight = flight;
     }
-
+    public Booking(int idBooking, List<Passenger> passenger, String flight, Pair pair) {
+        this.idBooking = idBooking;
+        this.passenger = passenger;
+        this.flight = flight;
+        this.pair = pair;
+    }
 
     public String displayBookingInfo() {
         return String.format("| %-10d |%-20s |%-10s |",
